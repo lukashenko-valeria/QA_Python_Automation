7b28af903775162a8623360b73b92f491ad970d8
lukashenko-valeria <lukashenko.valeria@mail.ru>
2025-07-16 20:41:43 +0500

    Add Sprint 5 scripts

---
+++ Sprint_5/consts.py
@@ -0,0 +1,11 @@
+STD_TEST_EMAIL = "v.lukashenko@gmail.com"
+STD_TEST_PASSWORD = "busina"
+
+WRONG_TEST_EMAIL = 'v.lukashenko@gmail'
+
+AD_TEST_EMAIL = "v.lukashenko23@gmail.com"
+AD_TEST_PASSWORD = "busina23"
+
+ADVERTISEMENT_NAME = "Приключения Шерлока Холмса"
+ADVERTISEMENT_PRICE = "560"
+ADVERTISEMENT_DESCRIPTION = "Книга в отличном состоянии"
